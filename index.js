d5627bbd54b30af059371766b63747796b141bc6
Sammy Kroner <sammyk118@gmail.com>
2021-01-19 20:07:18 -0800

    whoops

--- index.js
+++ index.js
@@ -18,29 +18,10 @@ const promptUser = () =>
     { type: 'input', name: 'email', message: 'Email address:' },
   ]);
 
-const generateREADME = (data) =>
-  `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My name is ${data.name}</h1>
-    <p class="lead">I am from ${data.location}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${data.github}</li>
-      <li class="list-group-item">LinkedIn: ${data.linkedin}</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>`;
+const generateREADME = (answers) =>
+  `
+  
+  `;
 
 
 async function init() {
